ec60775418af7a21cb22e453a893cbf6b1ae0718
astro-phel <katgranads@gmail.com>
2025-06-02 22:31:32 -0700

    Update OnlinePurchaseManager.java

--- src/main/java/OnlinePurchaseManager.java
+++ src/main/java/OnlinePurchaseManager.java
@@ -9,25 +9,27 @@ public class OnlinePurchaseManager
   /** Returns the number of purchased Gizmo objects that are electronic
   * whose manufacturer is maker, as described in part (a).
   */
-  public int countElectronicsByMaker(String maker)
-  {
-    /* to be implemented in part (a) */
-    int num = 0;
-    for(Gizmo g : purchases)
-      if(g.isElectronic() && g.getMaker().equals(maker))
-        num++;
-    return num;
+   public int countElectronicsByMaker(String maker) {
+       /* to be implemented in part (a) */
+      int count = 0;
+      for (Gizmo g : purchases) {
+          if (g.isElectronic() && g.getMaker().equals(maker)) {
+              count++;
+          }
+      }
+      return count;
   }
   
   /** Returns true if any pair of adjacent purchased Gizmo objects are
   * equivalent, and false otherwise, as described in part (b).
   */
-  public boolean hasAdjacentEqualPair()
-  {
-    /* to be implemented in part (b) */
-    for(int i = 0; i < purchases.size() - 1; i++)
-      if(purchases.get(i).equals(purchases.get(i+1)))
+  public boolean hasAdjacentEqualPair() {
+     /* to be implemented in part (b) */
+    for (int i = 0; i < purchases.size() - 1; i++) {
+      if (purchases.get(i).equals(purchases.get(i + 1))) {
         return true;
+      }
+    }
     return false;
   }
 }
