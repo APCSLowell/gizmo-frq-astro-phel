1ac7317ec7af1ddb902d2aa8a5d50101a92024a1
astro-phel <katgranads@gmail.com>
2025-06-02 22:50:32 -0700

    Update OnlinePurchaseManager.java

--- src/main/java/OnlinePurchaseManager.java
+++ src/main/java/OnlinePurchaseManager.java
@@ -1,4 +1,5 @@
 import java.util.ArrayList;
+
 public class OnlinePurchaseManager
 {
   /** An ArrayList of purchased Gizmo objects,
@@ -6,15 +7,10 @@ public class OnlinePurchaseManager
   */
   private ArrayList<Gizmo> purchases;
 
-  public OnlinePurchaseManager(){
-    purchases = new ArrayList<Gizmo>();
-  }
-  
   /** Returns the number of purchased Gizmo objects that are electronic
   * whose manufacturer is maker, as described in part (a).
   */
-   public int countElectronicsByMaker(String maker) {
-       /* to be implemented in part (a) */
+  public int countElectronicsByMaker(String maker) {
       int count = 0;
       for (Gizmo g : purchases) {
           if (g.isElectronic() && g.getMaker().equals(maker)) {
@@ -23,21 +19,16 @@ public class OnlinePurchaseManager
       }
       return count;
   }
-  
+
   /** Returns true if any pair of adjacent purchased Gizmo objects are
   * equivalent, and false otherwise, as described in part (b).
   */
   public boolean hasAdjacentEqualPair() {
-     /* to be implemented in part (b) */
-    for (int i = 0; i < purchases.size() - 1; i++) {
-      if (purchases.get(i).equals(purchases.get(i + 1))) {
-        return true;
+      for (int i = 0; i < purchases.size() - 1; i++) {
+          if (purchases.get(i).equals(purchases.get(i + 1))) {
+              return true;
+          }
       }
-    }
-    return false;
-  }
-
-  public void addPurchase(Gizmo g){
-    purchases.add(g);
+      return false;
   }
 }
